84bb6f4253fe7c1663b070a8622facade1ae65a3
chris <chris>
2017-12-15 21:15:01 +0100

    Initial commit of running hello world project with mvn package working

---
+++ src/main/java/com/github/chrissssss/docker/HelloDockerMain.java
@@ -0,0 +1,7 @@
+package com.github.chrissssss.docker;
+
+public class HelloDockerMain {
+    public static void main(String[] args) {
+        System.out.println("Hello World! This is java. I'm speaking from docker here! Really!");
+    }
+}
